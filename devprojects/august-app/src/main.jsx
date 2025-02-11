f30288dc16af2b420cf03791660fac60443fd91f
lisa <74734294+lisadesai@users.noreply.github.com>
2025-02-11 11:19:30 -0500

    Delete devprojects/august-app directory

--- devprojects/august-app/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App.jsx'
-import './index.css'
-import { ApolloClient, InM
